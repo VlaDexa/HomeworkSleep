d27d291c5a9d93669cf82b08bcf505a22b870738
Vlad <vgrechannik@gmail.com>
2019-12-10 22:09:41 +0300

    dfgh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+// tslint:disable-next-line: max-line-length
+import { Component, OnInit, OnChanges, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy, Input, SimpleChanges } from '@angular/core';
+// tslint:disable-next-line: no-conflicting-lifecycle
 
+// tslint:disable-next-line: no-conflicting-lifecycle
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template: `
+        <h1>Welcome to {{title}}!</h1>
+        <button (click)="increment()">Increment</button>
+        <button (click)="decrement()">decrement</button>
+        <app-input [count]=Counter></app-input>` ,
 })
-export class AppComponent {
+// tslint:disable-next-line: max-line-length
+export class AppComponent implements OnInit, OnChanges, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
+  constructor() {
+  }
   title = 'hek';
+  Counter = 5;
+
+  increment() {
+    this.Counter++;
+  }
+  decrement() {
+    this.Counter--;
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('changes');
+  }
+  ngAfterContentInit() {
+
+  }
+  ngAfterContentChecked() {
+
+  }
+  ngAfterViewInit() {
+
+  }
+  ngAfterViewChecked() {
+
+  }
+  ngOnInit() {
+    console.log('hell na');
+  }
+  ngOnDestroy() {
+
+  }
 }
+
+
--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
-  styleUrls: ['./input.component.css']
+  styleUrls: ['./input.component.css'],
+  template: `<h2>Child Component</h2>
+    current count is {{ count }}
+  `
 })
-export class InputComponent implements OnInit {
-
+export class InputComponent implements OnInit, OnChanges {
+  @Input() count: number;
   constructor() { }
 
   ngOnInit() {
   }
-
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('changes');
+  }
 }
